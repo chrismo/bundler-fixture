2fcecdec5ef5899320eb19b2e6e5acef513ec756
chrismo <chrismo@clabs.org>
2019-01-10 01:31:26 -0600

    only-bundler.sh: skip rubygems if no version given
    And install latest Bundler if no Bundler ver given.

--- only-bundler.sh
+++ only-bundler.sh
@@ -5,13 +5,24 @@
 # including default versions installed automatically by RubyGems upgrades or
 # downgrades.
 
+# NOTE: Downgrading RubyGems to some older version won't work if it's too old
+# for the current version of Ruby. You can find the RubyGems version installed
+# with Ruby at https://github.com/ruby/ruby/blob/ruby_2_4/lib/rubygems.rb
+
+set -x
+
 GEMS_DIR=$(ruby -e 'puts Gem.default_dir')
 SPEC_DIR=${GEMS_DIR}/specifications
 SITE_DIR=$(ruby -e 'puts RbConfig::CONFIG["sitelibdir"]')
+BUNDLER_VER="$1"
+RUBYGEMS_VER="$2"
 
 # Newer RubyGems versions will install a default Bundler, this needs to happen
 # before uninstalling all Bundler versions.
-gem update --system $2
+if [[ -n "$RUBYGEMS_VER" ]]
+then
+  gem update --system ${RUBYGEMS_VER}
+fi
 
 # Show existing Bundler versions
 gem list -e bundler -d
@@ -19,17 +30,22 @@ gem list -e bundler -d
 # Move bundler gemspec from default to reg directory so it can be uninstalled.
 mv -v ${SPEC_DIR}/default/bundler-*.gemspec ${SPEC_DIR}/
 
-# for dir in $(echo $GEM_PATH | tr ':' ' '); do gem uninstall -i $dir bundler -a -x; done
+for dir in $(echo ${GEM_PATH} | tr ':' ' '); do gem uninstall -i ${dir} bundler -a -x --force; done
 gem uninstall bundler -a -x --force
-gem uninstall -i ${GEMS_DIR} bundler -a -x
+gem uninstall -i ${GEMS_DIR} bundler -a -x --force
 
 # Newer RubyGems updates put Bundler here. Uninstalling isn't removing.
 rm -rf ${SITE_DIR}/bundler*
 
-gem install bundler -v $1 --no-document
+if [[ -n "$BUNDLER_VER" ]]
+then
+  gem install bundler -v ${BUNDLER_VER} --no-document
+else
+  gem install bundler --no-document
+fi
 
 # This may seem redundant, but sometimes is helpful when debugging.
 gem list -e bundler -d
 which bundle
 gem -v
-bundle --version
+bundle -v
